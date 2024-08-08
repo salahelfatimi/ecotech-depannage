fe4668899723ab28c2eb13922ceee5c438b7f45d
salahelfatimi <salahfatimi16@gmail.com>
2024-08-08 13:41:57 +0100

    add

--- src/components/phoneIcon.jsx
+++ src/components/phoneIcon.jsx
@@ -3,7 +3,7 @@ import { Phone } from "lucide-react";
 export default function PhoneIcon(){
     return(
         <div className=" z-10 flex gap-2 items-center  animate-bounce  fixed bottom-4 right-4 ">
-        <span className="  text-sm hidden lg:block text-center bg-[#0276FF] border-white border-4 text-white rounded-full items-center justify-center p-2">
+        <span className="  text-sm  text-center bg-[#0276FF] border-white border-4 text-white rounded-full items-center justify-center p-2">
             <div>Avez vous besoin d&apos;aide? </div>
           <strong>Contactez-nous</strong>
         </span>
