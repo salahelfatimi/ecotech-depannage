ec2af4f282bf16105789c755ae9470651b8a6c7e
salahelfatimi <salahfatimi16@gmail.com>
2024-08-08 13:36:53 +0100

    add

--- src/components/phoneIcon.jsx
+++ src/components/phoneIcon.jsx
@@ -4,7 +4,7 @@ export default function PhoneIcon(){
     return(
         <div className=" z-10 flex gap-2 items-center  animate-bounce  fixed bottom-4 right-4 ">
         <span className="  text-sm hidden lg:block text-center bg-[#0276FF] border-white border-4 text-white rounded-full items-center justify-center p-2">
-            <div>Avez vous besoin d'aide? </div>
+            <div>Avez vous besoin d&apos;aide? </div>
           <strong>Contactez-nous</strong>
         </span>
         <div className="bg-green-600 w-fit p-4 rounded-full ">
