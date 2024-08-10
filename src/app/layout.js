8d171c0a5ca20cb29b479a9382fb25a7a6660828
salahelfatimi <salahfatimi16@gmail.com>
2024-08-10 16:59:45 +0100

    add seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,12 +6,12 @@ import Footer from "@/components/footer/footer";
 const fredoka = Fredoka({ subsets: ["hebrew"] });
 
 export const metadata = {
-  title: "EcoTech Dépannage | Urgence Plombier, Électricien, Serrurier 24/7",
+  title: "EcoTech Dépannage | Dépannage d'urgence 24h/24 et 7j/7",
   description: "EcoTech Dépannage, votre expert en dépannage à Sarreguemines, France. Vitrier, serrurier, plombier, et électricien, disponibles 7J/7, 24h/24 pour des interventions d'urgence. Contactez-nous au +33 7 71 71 05 13 pour un service rapide.",
   metadataBase: new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`,),
-  keywords: ["plombier urgence,Dépannage plomberie,urgence plomberie,urgence plombier,plombier Sarreguemines,Dépannage Sarreguemines, vitrier Sarreguemines, serrurier Sarreguemines, plombier Sarreguemines, électricien Sarreguemines, intervention d'urgence, service 24h/24, réparation écologique, EcoTech Dépannage."],
+  keywords: ["plombier urgence,Dépannage plomberie,urgence plomberie,urgence plombier,plombier Sarreguemines,Dépannage Sarreguemines, vitrier Sarreguemines, serrurier Sarreguemines, plombier Sarreguemines, électricien Sarreguemines, intervention d'urgence, service 24h/24, EcoTech Dépannage,dépannage d'urgence 24h/24, dépannage 7j/7 Sarreguemines, plombier urgence Sarreguemines, serrurier urgence Sarreguemines, vitrier Sarreguemines urgence", "électricien Sarreguemines 24h/24, intervention rapide Sarreguemines, EcoTech Dépannage urgence, réparation écologique Sarreguemines"],
   openGraph: {
-    title: 'EcoTech Dépannage | Urgence Plombier, Électricien, Serrurier 24/7',
+    title: "EcoTech Dépannage | Dépannage d'urgence 24h/24 et 7j/7",
     description: "EcoTech Dépannage, votre expert en dépannage à Sarreguemines, France. Vitrier, serrurier, plombier, et électricien, disponibles 7J/7, 24h/24 pour des interventions d'urgence. Contactez-nous au +33 7 71 71 05 13 pour un service rapide.",
     url:`${process.env.NEXT_PUBLIC_BASE_URL}`,
     siteName: "EcoTech Dépannage",
@@ -21,7 +21,7 @@ export const metadata = {
         secureUrl: `/opengraph-image.jpg`,
         width: 1200,
         height: 630,
-        alt: `EcoTech Dépannage | Urgence Plombier, Électricien, Serrurier 24/7`,
+        alt: `EcoTech Dépannage | Dépannage d'urgence 24h/24 et 7j/7`,
       }
     ],
     type: "website",
