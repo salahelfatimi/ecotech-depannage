b6ae58316718063d47738f4016231864a67f7c94
salahelfatimi <salahfatimi16@gmail.com>
2024-08-10 18:01:30 +0100

    addd more function

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 export default function Footer(){
     return(
-      <footer className="flex flex-col" >
+      <footer className="flex flex-col z-20" >
         <div className="bg-[#0276FF] py-8 ">
             <div className=" flex flex-col gap-4 lg:flex-row items-center justify-between  container ">
            
-            <Link href={'#'} className=" flex gap-4"><MapPin size={30} className=" stroke-[#fff]" /><span className=" text-xl items-center font-semibold capitalize text-center lg:text-start text-white ">Sarreguemines, France</span></Link>
-            <Link href={'mailto:ecotech-depannage@gmail.com'} className=" flex gap-4"><Mail size={30} className=" stroke-[#fff]" /><span className=" text-xl items-center font-semibold  text-center lg:text-start text-white ">services-reparation@gmail.com</span></Link>
+            <Link href={'https://maps.app.goo.gl/gtaWxSsq7yA2k8fk9'} target="_blank" className=" flex gap-4"><MapPin size={30} className=" stroke-[#fff]" /><span className=" text-xl items-center font-semibold capitalize text-center lg:text-start text-white ">Sarreguemines, France</span></Link>
+            <Link href={'mailto:ecotech-depannage@gmail.com'}  className=" flex gap-4"><Mail size={30} className=" stroke-[#fff]" /><span className=" text-xl items-center font-semibold  text-center lg:text-start text-white ">services-reparation@gmail.com</span></Link>
             <Link href={'tel:+33771710513'} className=" flex gap-4"><Phone size={30} className=" stroke-[#fff]" /><span className=" text-xl items-center font-semibold capitalize text-center lg:text-start text-white ">+33 7 71 71 05 13</span></Link>
 
             
