1f24c187e4ae950c7cdf34251eea99c464a2eb9f
salahelfatimi <salahfatimi16@gmail.com>
2024-09-01 17:37:09 +0100

    add

--- src/components/tools/phoneIcon.jsx
+++ src/components/tools/phoneIcon.jsx
@@ -6,9 +6,9 @@ export default function PhoneIcon(){
         <div className=" z-10 flex gap-2 items-center  animate-bounce  fixed bottom-4 right-4 ">
         <span className="   text-sm  text-center text-[#0276FF] select-none  bg-[#ebf0f8] rounded-full items-center justify-center py-2 px-4">
             <div>Avez vous besoin d&apos;aide? </div>
-          <strong>Contactez-nous</strong>
+          <strong>Appelez-nous</strong>
         </span>
-        <Link href="https://wa.me/33771710513" target="_blank" className="bg-green-600 w-fit p-4 rounded-full ">
+        <Link href={'tel:+33771710513'} className="bg-green-600 w-fit p-4 rounded-full ">
           <Phone size={25} className=" stroke-white stroke-2"/>
         </Link>
         
