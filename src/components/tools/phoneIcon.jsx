98a9d61e8e7a340d6e00f106cc3bbc31e316295c
salahelfatimi <salahfatimi16@gmail.com>
2024-12-08 16:22:15 +0100

    add

--- src/components/tools/phoneIcon.jsx
+++ src/components/tools/phoneIcon.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function PhoneIcon(){
     return(
-        <div className=" z-50 flex gap-2 items-center  animate-bounce  fixed bottom-4 right-4 ">
+        <div className=" z-40 flex gap-2 items-center  animate-bounce  fixed bottom-4 right-4 ">
         <span className="   text-sm  text-center text-[#0276FF] select-none  bg-[#ebf0f8] rounded-full items-center justify-center py-2 px-4">
             <div>Avez vous besoin d&apos;aide? </div>
             <strong>Messagez-nous</strong>
