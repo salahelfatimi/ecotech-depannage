93d596373aecb6a0381b8367099f5634ba881b35
salahelfatimi <salahfatimi16@gmail.com>
2024-12-08 16:13:06 +0100

    add

--- src/components/pages/realisations.jsx
+++ src/components/pages/realisations.jsx
@@ -8,12 +8,13 @@ export default function Realisations(){
             <div className=" flex flex-col gap-4 ">
                 <h2 className="font-bold text-3xl xl:text-6xl text-white text-center">Nos Realisations</h2>
                 <p className=" p-4 text-center text-2xl xl:text-3xl text-white ">Explorez notre savoir-faire à travers nos réalisations.</p>
-                <div className="  ">
-                    <RealisationsCarousel direction='backward' lengthImage={1} limiteImage={14}/>
+                <div className="mb-10">
+                    <RealisationsCarousel direction="backward" startImage={1} limiteImage={14} />
                 </div>
-                <div className="  ">
-                    <RealisationsCarousel direction='forward' lengthImage={15} limiteImage={14}/>
+                <div className="mb-10">
+                    <RealisationsCarousel direction="forward" startImage={15} limiteImage={14} />
                 </div>
+
             </div>
            
         </div>
