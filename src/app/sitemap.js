dd4636b442869558e8d91e164f592c5f2dbe3c24
salahelfatimi <salahfatimi16@gmail.com>
2024-12-08 16:33:35 +0100

    add

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,17 +1,13 @@
-export default async function sitemap() {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-  const imageCount = 28; // Total number of images
 
-  const imageEntries = Array.from({ length: imageCount }, (_, i) => ({
-    url: `${baseUrl}/realisations/${i + 1}.jpeg`,
-    title: `Realisation ${i + 1}`, // Optional
-  }));
+
+export default async function sitemap() {
+ 
 
   return [
     {
-      url: `${baseUrl}`,
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
       lastModified: new Date(),
-      images: imageEntries,
-    },
+    }
+    
   ];
 }
