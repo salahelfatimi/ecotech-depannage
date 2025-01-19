faeab661f86aed7b79591f4afbd284780a0c9a8a
salahelfatimi <salahfatimi16@gmail.com>
2025-01-19 15:29:25 +0100

    feat: update sitemap with additional URLs for contact and nos_service pages

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -7,7 +7,41 @@ export default async function sitemap() {
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
       lastModified: new Date(),
-    }
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/contact`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service/electricite`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service/plomberie`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service/climatisation`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service/chauffage`,
+      lastModified: new Date(),
+    },
+    ,
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service/serrurerie`,
+      lastModified: new Date(),
+    },
+    ,
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/nos_service/vitrerie_&_miroiterie`,
+      lastModified: new Date(),
+    },
     
   ];
 }
